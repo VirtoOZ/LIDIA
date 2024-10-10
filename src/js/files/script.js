6b44fe7bf8f1c565c08c4fcbcf8fb6688cbac3be
VirtoOZ <109903427+VirtoOZ@users.noreply.github.com>
2024-10-10 23:05:26 +0300

    Header Версия 2. Рабочая, Но корявая.

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -8,14 +8,17 @@ window.onload = function () {
 		const targetElement = e.target; // кладем в переменную нажатый объект
 		const iconMenu = document.querySelector('.icon-menu');
 		const menuBody = document.querySelector('.menu-body');
+		const wrapperHeader = document.querySelector('.header__wrapper');
 
 		if (targetElement == iconMenu) {
 			if (iconMenu.classList.contains('_active')) {
 				iconMenu.classList.remove('_active');
 				menuBody.classList.remove('_active');
+				wrapperHeader.classList.remove('_active');
 			} else {
 				iconMenu.classList.toggle('_active');
 				menuBody.classList.toggle('_active');
+				wrapperHeader.classList.toggle('_active');
 			}
 		}
 	}
