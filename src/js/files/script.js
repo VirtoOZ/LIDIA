f1ec7ac7520ff79191b78d4f54e51c0ccdd0b52a
VirtoOZ <CenTROY@yandex.ru>
2024-10-11 16:39:05 +0300

    Переписал Header.

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -7,18 +7,15 @@ window.onload = function () {
 	function documentActions(e) {
 		const targetElement = e.target; // кладем в переменную нажатый объект
 		const iconMenu = document.querySelector('.icon-menu');
-		const menuBody = document.querySelector('.menu-body');
-		const wrapperHeader = document.querySelector('.header__wrapper');
+		const header = document.querySelector('.header');
 
 		if (targetElement == iconMenu) {
 			if (iconMenu.classList.contains('_active')) {
 				iconMenu.classList.remove('_active');
-				menuBody.classList.remove('_active');
-				wrapperHeader.classList.remove('_active');
+				header.classList.remove('_menu-open');
 			} else {
 				iconMenu.classList.toggle('_active');
-				menuBody.classList.toggle('_active');
-				wrapperHeader.classList.toggle('_active');
+				header.classList.toggle('_menu-open');
 			}
 		}
 	}
